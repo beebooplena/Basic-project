bdcb6816004c7342599a19be98537019627dba7b
beebooplena <lena_kath@hotmail.com>
2022-04-05 13:19:08 +0000

    implement emailsjs integration on contact page

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,17 @@
+
+function sendMail(contactForm) {
+    emailjs.send("service_cgcveku", "template_fge6l5u", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;  // To block from loading a new page
+}
